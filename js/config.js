256ded839ba440381ac465252430de47b2d1abba
Fabio Dellutri <fabio.dellutri@mitecube.com>
2013-04-08 17:34:01 +0200

    v2.0 - beta

--- js/config.js
+++ js/config.js
@@ -1,2 +1,2 @@
-var elastic_search_url = 'http://33.33.33.101:9200';
-//var elastic_search_url = 'http://doveticuri.mitecube.com/search';
+//var elastic_search_url = 'http://33.33.33.101:9200';
+var elastic_search_url = 'http://doveticuri.mitecube.com/search';
